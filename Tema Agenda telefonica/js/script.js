8430335678a51c873a4221514440f7f58c80760c
gelu89 <ic.gelu@gmail.com>
2021-01-17 15:06:44 +0200

    hfghfgh

--- Tema Agenda telefonica/js/script.js
+++ Tema Agenda telefonica/js/script.js
@@ -145,14 +145,4 @@ document.addEventListener('invalid', (function () {
       e.preventDefault();
     };
   })(), true);
-
-
-
-// Loading circle animation with delay on form  animation (.left > form)
-let loader = document.querySelector(".anim");
-let loading = document.querySelector(".left");
-loading.classList.add('js-loading');
-window.addEventListener('load', function () {
-    loader.parentElement.removeChild(loader);
-    loading.classList.remove('js-loading');
-});                     
+                   
